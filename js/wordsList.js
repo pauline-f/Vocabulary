2209cdff1b523f56f5e1815dba73bd67cae074b6
Pauline <pauline.feytel@gmail.com>
2017-04-16 22:05:16 +0200

    Delete row in wordsList.html

--- js/wordsList.js
+++ js/wordsList.js
@@ -9,6 +9,7 @@ function createTd(text, parent) {
 
 for (var i = 0;i < words.length;i++) {
     var trTableElt = document.createElement("tr");
+    trTableElt.id = "tr" + i;
     document.getElementById("bodyTable").appendChild(trTableElt);
 
     createTd(i + 1, trTableElt);
@@ -21,6 +22,14 @@ for (var i = 0;i < words.length;i++) {
     tdTableElt.appendChild(deleteButtonElt);
 
     var deleteImgElt = document.createElement("img");
+    deleteImgElt.id = "bt" + i;
     deleteImgElt.src = "../images/delete.png";
     deleteButtonElt.appendChild(deleteImgElt);
-} 
+
+    deleteButtonElt.addEventListener("click", function(e) {
+        var tr = e.target.parentNode     // button
+                     .parentNode         // td
+                     .parentNode;        // tr
+        document.getElementById("bodyTable").removeChild(tr);
+    });
+}
